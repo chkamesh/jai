0e41e12691376eef36a1a956525f473a8953a7f7
kameshchitimoju <chitimoju.kamesh@gmail.com>
2019-09-11 22:34:25 +0530

    commit

--- J_Alpha/src/com/alpha/db/JEntity.java
+++ J_Alpha/src/com/alpha/db/JEntity.java
@@ -7,49 +7,61 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="JEntity")
+@Table(name = "JEntity")
 public class JEntity {
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String entId;
 	private String entName;
 	private String entDesc;
 	private String viewName;
 	private boolean isMaster;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getEntId() {
 		return entId;
 	}
+
 	public void setEntId(String entId) {
 		this.entId = entId;
 	}
+
 	public String getEntName() {
 		return entName;
 	}
+
 	public void setEntName(String entName) {
 		this.entName = entName;
 	}
+
 	public String getEntDesc() {
 		return entDesc;
 	}
+
 	public void setEntDesc(String entDesc) {
 		this.entDesc = entDesc;
 	}
+
 	public String getViewName() {
 		return viewName;
 	}
+
 	public void setViewName(String viewName) {
 		this.viewName = viewName;
 	}
+
 	public boolean isMaster() {
 		return isMaster;
 	}
+
 	public void setMaster(boolean isMaster) {
 		this.isMaster = isMaster;
 	}
