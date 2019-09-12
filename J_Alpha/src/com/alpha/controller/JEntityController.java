0de541c1725b572434babccfce8730403c519968
chitimoju kamesh <chitimoju.kamesh@gmail.com>
2019-09-12 10:06:52 +0530

    test

--- J_Alpha/src/com/alpha/controller/JEntityController.java
+++ J_Alpha/src/com/alpha/controller/JEntityController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.alpha.db.JEntity;
 import com.alpha.service.JEntityService;
-
+//
 @RestController
 public class JEntityController {
 
